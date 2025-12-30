d45232fa92b789722de8fd5378e7eee494911bb1
GunjanRanjan <gunjandulmi@gmail.com>
2025-12-30 04:17:13 +0000

    Test Case 2

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.[jt]sx?$/,
-    exclude: [],
-  },
   base: "/toDoApp/",
 });
